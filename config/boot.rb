df99e257215374663f001178820b2ef8fb80855d
Yew Wee <chuayewwee@gmail.com>
2019-01-01 13:13:31 +0800

    First commit

---
+++ config/boot.rb
@@ -0,0 +1,13 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge(Port: ENV.fetch('PORT', 4567))
+    end
+  end
+end
