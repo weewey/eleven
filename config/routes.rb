886abc3bbc37b90cb46f69328f1bd47e91000f5c
Yew Wee <chuayewwee@gmail.com>
2019-01-02 13:35:17 +0800

    scaffold photos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :photos
   get '/ping', to: 'health_check#health_check'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_061602) do
+ActiveRecord::Schema.define(version: 2019_01_02_020433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "race"
+    t.string "image_url"
+    t.string "tags", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,4 +51,5 @@ ActiveRecord::Schema.define(version: 2019_01_01_061602) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "photos", "users", on_delete: :cascade
 end
