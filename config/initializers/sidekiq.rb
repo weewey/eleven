4d96c2f2f88991ff737cd5565107b2a83ad2ebfc
yew wee <el.eleven>
2019-12-17 19:24:49 +0800

    remove sidekiq logging issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,5 @@
 require 'sidekiq/web'
 
-Sidekiq::Logging.logger.level = Logger::INFO
-
-#configure Sidekiq for dev environment
-if Rails.env.development?
-  Sidekiq::Logging.logger.level = Logger::DEBUG
-  Rails.logger = Sidekiq::Logging.logger
-  ActiveRecord::Base.logger = Sidekiq::Logging.logger
-end
-
 Sidekiq.configure_server do |config|
   config.redis = { url: Figaro.env.sidekiq_redis_url }
 end
@@ -18,6 +9,3 @@ Sidekiq.configure_client do |config|
 end
 
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
-
-# Trying to workaround the IPSpoofing issue when accessing /sidekiq via HAProxy
-Sidekiq::Web.use(::Rack::Protection, except: :ip_spoofing)
